af6fb959c284b73de4299bbae341d28009135c9a
ajnaspv <developer.ajnaspv.in@gmail.com>
2026-03-16 16:02:23 +0530

    Final commit

--- FlutterApp/Edupay/lib/mobile_razorpay_helper.dart
+++
@@ -1,8 +0,0 @@
-// This file is only here to satisfy conditional imports on mobile
-void openRazorpayWeb(
-    Map<String, dynamic> options, {
-      Function(String paymentId)? onSuccess,
-      Function(String error)? onError,
-    }) {
-  // No implementation needed for mobile
-}
