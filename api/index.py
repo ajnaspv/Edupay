816c46e5c4fd6a4d88260999886a330f3292254e
ajnaspv <developer.ajnaspv.in@gmail.com>
2026-03-16 16:59:40 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,12 @@
 import os
 import sys
-from django.core.wsgi import get_wsgi_application
 
+# add project root to python path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+# Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Edupay.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
