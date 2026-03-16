fdb5f8396d6d688f382efd5c49d26e201e472741
ajnaspv <developer.ajnaspv.in@gmail.com>
2026-03-16 16:49:11 +0530

    Final Commit

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edupay.settings")
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Edupay.settings")
 
 app = get_wsgi_application()
